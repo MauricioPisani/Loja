cd270e9c57f4ac1f891594a72b547f7a785ad2c4
mauricio <mauriciofatecads@hotmail.com>
2019-10-01 10:23:42 -0300

    versao 0.2.1=>sing up v0.2

--- app/src/main/java/br/edu/ifsul/loja/activity/CadUserActivity.java
+++ app/src/main/java/br/edu/ifsul/loja/activity/CadUserActivity.java
@@ -2,6 +2,9 @@ package br.edu.ifsul.loja.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import br.edu.ifsul.loja.R;
 
 
@@ -12,7 +15,9 @@ public class CadUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cad_user);
-
+        TextView tv_nome=findViewById(R.id.tv_nome_cadUser;
+        TextView tv_email=findViewById(R.id.tv_email_cadUser;
+        EditText ed_senha=findViewById(R.id.ed_senha_cadUser;
 
 
     }

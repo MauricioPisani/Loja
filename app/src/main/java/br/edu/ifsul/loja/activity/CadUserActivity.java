0c564d79092106b6a8308a4a3fff774b55f0771b
mauricio <mauriciofatecads@hotmail.com>
2019-10-01 09:16:17 -0300

    versao 0.2.1=>inicio do sing up

---
+++ app/src/main/java/br/edu/ifsul/loja/activity/CadUserActivity.java
@@ -0,0 +1,20 @@
+package br.edu.ifsul.loja.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import br.edu.ifsul.loja.R;
+
+
+public class CadUserActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_user);
+
+
+
+    }
+
+}
